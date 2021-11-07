e376df265b1b5d5f30eeb719c68aa4e8d01ded6e
Claire <claire@Claires-MacBook-Pro-3.local>
2021-11-07 14:29:41 -0500

    added highscore functions

--- assets/script.js
+++ assets/script.js
@@ -1,7 +1,30 @@
 var timer = document.querySelector("#timer");
 var startBtn = document.querySelector("#startBtn");
+var quiz = document.querySelector("#quiz");
+var startContent = document.querySelector("#startContent");
+var quizQuestion = document.querySelector("#quizQuestion");
+var choiceA = document.querySelector("#choiceA");
+var choiceB = document.querySelector("#choiceB");
+var choiceC = document.querySelector("#choiceC");
+var choiceD = document.querySelector("#choiceD");
+var correctness = document.querySelector("#correctness");
+var selectA = document.querySelector("#selectA");
+var selectB = document.querySelector("#selectB");
+var selectC = document.querySelector("#selectC");
+var selectD = document.querySelector("#selectD");
+var endContent = document.querySelector("#endContent");
+var finalScore = document.querySelector("#finalScore");
+var userInitials = document.querySelector("#userInitials");
+var scoreSubmit = document.querySelector("#scoreSubmit");
+var highScoreLink = document.querySelector("#highScoreLink");
+var savedScoresContent = document.querySelector("#savedScoresContent");
+var highScoreContent = document.querySelector("#highScoreContent");
+var goBackButton = document.querySelector("#goBack");
+var count = 0;
+var timeLeft = 90;
+var score = 0;
 
-var questions = [
+var myQuestions = [
     {
         question: "Which of the following is NOT an example of a semantic HTML element?",
         answers: {
@@ -105,24 +128,126 @@ var questions = [
 ];
 
 function countdown() {
-    var timeLeft = 60;
-
     var timeInterval = setInterval(function() {
         if (timeLeft > 0) {
             timer.textContent = "Time: " + timeLeft;
             timeLeft--;
+        } else if (startContent.style.display === 'block' || highScoreContent.style.display === 'block') {
+            clearInterval(timeInterval);
         } else {
-            timer.textContent = "Time's Up!"
-            // run end quiz function here ?
+            clearInterval(timeInterval);
+            endQuiz();
         }
     }, 1000);
 };
 
 function startQuiz() {
-    // run countdown at the start of start quiz function
+    timeLeft = 90;
+    // start timer
     countdown();
 
+    startContent.style.display = 'none';
+    startBtn.style.display = 'none';
+    quiz.style.display = 'block';
 
+    showQuestions();
 };
+
+function showQuestions() {
+    quizQuestion.textContent = myQuestions[count].question;
+    choiceA.textContent = myQuestions[count].answers.a;
+    choiceB.textContent = myQuestions[count].answers.b;
+    choiceC.textContent = myQuestions[count].answers.c;
+    choiceD.textContent = myQuestions[count].answers.d;
+
+    count++;
+};
+
+function isCorrect() {
+    var userAnswer = false; 
+
+    document.addEventListener("click", function(event) {
+        userAnswer = event.target;
+    });
+
+    if (userAnswer === myQuestions[count].correctAnswer) {
+        correctness.textContent = "Correct";
+        buttonClicked = true;
+    } else if (userAnswer) {
+        correctness.textContent = "Incorrect";
+        buttonClicked = true;
+    }
+};
+
+function endQuiz() {
+    timer.style.display = 'none';
+    quiz.style.display = 'none';
+    endContent.style.display = 'block';
+    
+    // show score
+    finalScore.textContent = "Your final score is: " + score;
+};
+
+function saveScores() {
+    var i = 0;
+    var savedScores = [
+        {
+            initials: "",
+            userScore: ""
+        }
+    ];
+
+    savedScores[i].initials = localStorage.getItem("initials");
+    savedScores[i].userScore = score;
+
+    savedScoresContent.textContent = savedScoresContent.textContent + savedScores[i].initials + ":  " + savedScores[i].userScore;
+
+    console.log(savedScores[i]);
+
+    i++;
+};
+
+document.addEventListener("click", function(event) {
+    if (event.target === selectA || event.target === selectB || event.target === selectC || event.target === selectD) {
+        if (event.target.textContent === myQuestions[count - 1].correctAnswer) {
+            correctness.textContent = "Correct!";
+            score = score + 10;
+        } else {
+            correctness.textContent = "Incorrect";
+            timeLeft = timeLeft - 10;
+        }
+        if (count < myQuestions.length) {
+            showQuestions();
+        } else {
+            endQuiz();
+        }
+    }
+});
+
+scoreSubmit.addEventListener("click", function() {
+    if (userInitials.value) {
+        localStorage.setItem("initials", userInitials.value);
+        userInitials.value = "";
+        saveScores();
+    } else {
+        window.alert("Please type your initials.");
+        endQuiz();
+    }
+});
+
+highScoreLink.addEventListener("click", function() {
+    startContent.style.display = 'none';
+    quiz.style.display = 'none';
+    endContent.style.display = 'none';
+    highScoreContent.style.display = 'block';
+});
+
+goBackButton.addEventListener("click", function() {
+    startContent.style.display = 'block';
+    startBtn.style.display = 'block';
+    quiz.style.display = 'none';
+    endContent.style.display = 'none';
+    highScoreContent.style.display = 'none';
+});
 
 startBtn.addEventListener("click", startQuiz);
